8fd0e9347682c95774ebe36546a5af4fb49eb9f2
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-01-03 00:27:34 +0300

    Update UpdateContactForm.jsx

--- src/components/UpdateContactForm/UpdateContactForm.jsx
+++ src/components/UpdateContactForm/UpdateContactForm.jsx
@@ -1,10 +1,11 @@
-import { useState } from 'react';
 import { useDispatch } from 'react-redux';
+import { useForm } from 'react-hook-form';
 import toast from 'react-hot-toast';
 import PropTypes from 'prop-types';
 import { updateContact } from 'redux/contacts/operations';
 import { useContacts } from 'hooks/useContacts';
 import { Button } from 'components/Button';
+import { ErrorMessage } from 'components/ErrorMessage';
 import { Form, Label, Input } from './StyledUpdateContactForm';
 
 export const UpdateContactForm = ({
@@ -14,31 +15,22 @@ export const UpdateContactForm = ({
   closeModal,
 }) => {
   const { isLoading, error } = useContacts();
-
-  const [name, setName] = useState(contactName);
-  const [number, setNumber] = useState(contactNumber);
   const dispatch = useDispatch();
+  const {
+    register,
+    handleSubmit,
+    getValues,
+    formState: { errors },
+    clearErrors,
+  } = useForm({
+    defaultValues: {
+      name: contactName,
+      number: contactNumber,
+    },
+  });
 
-  const handleChangeInput = event => {
-    const { name, value } = event.target;
-    switch (name) {
-      case 'name':
-        setName(value);
-        break;
-      case 'phone':
-        setNumber(value);
-        break;
-
-      default:
-        return;
-    }
-  };
-
-  const handleSubmit = event => {
-    event.preventDefault();
-    setName(name);
-    setNumber(number);
-
+  const handleUpdateContact = data => {
+    const { name, number } = getValues();
     dispatch(updateContact({ contactId, name, number }));
 
     closeModal();
@@ -49,41 +41,69 @@ export const UpdateContactForm = ({
     toast.success(`Update contact ${name} `);
   };
 
-  const clearInput = name === '' || number === '';
-
   return (
-    <Form onSubmit={handleSubmit}>
+    <Form onSubmit={handleSubmit(handleUpdateContact)}>
       <Label htmlFor="nameId">
         Name
         <Input
+          {...register('name', {
+            required: 'Username is required.',
+            pattern: {
+              value:
+                /^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/,
+              message:
+                'It may contain only letters, apostrophe, dash and spaces.',
+            },
+            minLength: {
+              value: 2,
+              message: 'Minimal length is 6 symbols',
+            },
+            maxLength: {
+              value: 36,
+              message: 'Maximal length is 36 symbols',
+            },
+          })}
           id="nameId"
-          type="text"
-          name="name"
-          value={name}
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-          required
-          onChange={handleChangeInput}
+          tape="text"
           placeholder=" "
+          onChange={() => clearErrors('name')}
         />
+        {errors.name && <ErrorMessage>{errors.name?.message}</ErrorMessage>}
       </Label>
 
-      <Label htmlFor="telId">
-        Phone
+      <Label htmlFor="numberId">
+        Phone number
         <Input
-          id="telId"
+          {...register('number', {
+            required: 'Phone number is required.',
+            pattern: {
+              value:
+                /\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}/,
+
+              message:
+                'It must be digits and can contain spaces, dashes, parentheses.',
+            },
+
+            minLength: {
+              value: 6,
+              message: 'Minimal length is 6 symbols',
+            },
+            maxLength: {
+              value: 21,
+              message: 'Maximal length is 21 symbols',
+            },
+          })}
+          id="numberId"
           type="tel"
-          name="phone"
-          value={number}
-          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-          required
-          onChange={handleChangeInput}
           placeholder=" "
+          onChange={() => clearErrors('number')}
         />
+        {errors.number && <ErrorMessage>{errors.number?.message}</ErrorMessage>}
       </Label>
 
-      <Button disabled={isLoading || clearInput}>Update contact</Button>
+      <Button disabled={errors.name || errors.number || isLoading}>
+        Update contact
+      </Button>
     </Form>
   );
 };
