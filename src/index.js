3e3d4a384606e72f943ed0f8d3bd0fe97f7b2085
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-01-09 00:23:38 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <ThemeProvider theme={theme}>
-          <BrowserRouter basename="/organizer">
+          <BrowserRouter basename="/phonebook">
             <GlobalStyles />
             <App />
           </BrowserRouter>
