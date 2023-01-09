041c614e695e005aa01560ffd7d87fb1a404471a
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-01-09 23:28:07 +0300

    Create AboutApplicatin.jsx

---
+++ src/components/AboutApplicatin/AboutApplicatin.jsx
@@ -0,0 +1,19 @@
+import { Title, Text } from './StyledAboutApplicatin';
+
+export const AboutApplicatin = () => {
+  return (
+    <>
+      <Title>PHONEBOOK</Title>
+
+      <Text>
+        This application allows you to create a list of contacts (name and phone
+        number).
+      </Text>
+      <Text>
+        You can add new contacts, delete unnecessary contacts, edit them and
+        find the desired contact by name.
+      </Text>
+      <Text>To start work, log in.</Text>
+    </>
+  );
+};
